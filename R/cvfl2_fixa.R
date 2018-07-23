fd49bf6aa8a364c09ce7c6fba50d50eb664e5157
Marie Perrot-Dockes <marie.perrot-dockes@agroparistech.fr>
2018-07-23 11:16:15 +0200

    Commit  cv fl2

--- R/cvfl2_fixa.R
+++ R/cvfl2_fixa.R
@@ -14,35 +14,9 @@
 #'y <- X%*%B + rnorm(20)
 #'y <- scale(y)
 #'mod1 <- bic.fl2(y, regressors, group)
-#'mod <- stab.fl2_fixa(y, regressors, group, a = mod1[[3]])
+#'mod <- cv_fl2_fixa(y, regressors, group, a = mod1[[3]])
 #'coef(mod, s='lambda.min')
 #' @export
-# stab.fl2_fixa <- function(response, regressors, group, a,
-#                    lambda = NULL,  nrep= 1000,
-#                    nb.cores = 3, plot = TRUE){
-#   p <- ncol(regressors)
-#   X  <- model.matrix(~group + group:regressors - 1)
-#   if(is.null(colnames(regressors))) colnames(regressors) <- paste0('regressors', 1:p)
-#   X2 <- cbind(X,regressors)
-#   b <- (3 * p - a * p + 2) / (2 * p)
-#   q <- max(5, min(2 * round(nrow(X)/log(ncol(X))/10)*10, ncol(X)))
-#   mod1 <- cv.glmnet(x = X2, y = response, standardize = FALSE,
-#                    penalty.factor = c(0, 0, rep(b, (ncol(X) - 2)), rep(a, ncol(regressors))),
-#                    intercept = F, lambda = lambda )
-#
-#   coef     <- coef(mod1, s="lambda.min")
-#   reg_full <- names(coef[colnames(regressors),])[coef[colnames(regressors),] != 0]
-#   reg_mod  <- colnames(X)[!(apply(sapply(reg_full,function(x){grepl(paste0(x,"$"),colnames(X))}), 1, sum)) |
-#                               coef[colnames(X),] !=0 ]
-#
-#   mod <- stabsel(X2[,c(reg_mod,reg_full)], y, q=q, PFER=1, B = nrep,
-#                  fitfun = glmnet.lasso,
-#                  args.fitfun = list(standardize=FALSE), mc.cores = nb.cores, mc.preschedule = TRUE)
-#
-#   return(mod)
-# }
-
-
 cv_fl2_fixa <- function(response, regressors, group, a,
          lambda = NULL,  nrep= 1000,
          nb.cores = 3, plot = TRUE){
